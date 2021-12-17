6568cf11248044c76521612b9d776120c9d492b6
Frawley <frawleyskid@gmail.com>
2021-12-16 22:56:19 -0500

    Parse bibtex files for config patterns

--- config/pattern/values.go
+++ config/pattern/values.go
@@ -1,5 +1,15 @@
 package pattern
 
+import (
+	"errors"
+	"fmt"
+	"github.com/nickng/bibtex"
+	"net/url"
+	"strings"
+)
+
+var ErrInvalidReferenceUrl = errors.New("invalid URL from bibtex file")
+
 type EntryNameValues struct {
 	Doi     *string
 	Title   *string
@@ -8,7 +18,31 @@ type EntryNameValues struct {
 	EntryId *string
 }
 
-func (e *EntryNameValues) Parser() Parser {
+func entryField(entry bibtex.BibEntry, field string) *string {
+	if value, ok := entry.Fields[field]; ok {
+		stringValue := value.String()
+		return &stringValue
+	}
+
+	return nil
+}
+
+func NewEntryNameValues(entry bibtex.BibEntry) *EntryNameValues {
+	values := &EntryNameValues{}
+
+	values.Doi = entryField(entry, "doi")
+	values.Title = entryField(entry, "title")
+	if date := entryField(entry, "date"); date != nil {
+		// The date field should be in YYYY-MM-DD format.
+		values.Year = &strings.SplitN(*date, "-", 2)[0]
+	}
+	values.Authors = entryField(entry, "author")
+	values.EntryId = &entry.CiteName
+
+	return values
+}
+
+func (e EntryNameValues) Parser() Parser {
 	return NewParser(map[Var]*string{
 		'd': e.Doi,
 		't': e.Title,
@@ -24,10 +58,29 @@ type ProxySchemaValues struct {
 	Path     *string
 }
 
-func (e *ProxySchemaValues) Parser() Parser {
+func (e ProxySchemaValues) Parser() Parser {
 	return NewParser(map[Var]*string{
 		'd': e.Doi,
 		'h': e.Hostname,
 		'p': e.Path,
 	})
+}
+
+func NewProxySchemaValues(entry bibtex.BibEntry) (*ProxySchemaValues, error) {
+	values := &ProxySchemaValues{}
+
+	values.Doi = entryField(entry, "doi")
+
+	if rawUrl := entryField(entry, "url"); rawUrl != nil {
+		entryUrl, err := url.Parse(*rawUrl)
+		if err != nil {
+			return nil, fmt.Errorf("%w: %v", ErrInvalidReferenceUrl, err)
+		}
+
+		urlPath := strings.TrimPrefix(entryUrl.Path, "/")
+		values.Hostname = &entryUrl.Host
+		values.Path = &urlPath
+	}
+
+	return values, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/frawleyskid/ipfs-bib
 go 1.17
 
 require (
+	github.com/nickng/bibtex v1.0.3
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.17.0
 	github.com/pelletier/go-toml v1.9.4
@@ -11,21 +12,12 @@ require (
 
 require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/magiconair/properties v1.8.5 // indirect
-	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
-	github.com/spf13/afero v1.6.0 // indirect
-	github.com/spf13/cast v1.4.1 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.10.0 // indirect
-	github.com/subosito/gotenv v1.2.0 // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
 	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
 	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
