ef6b23a3ba059fd36fe3bcf911142ec9289e9109
Frawley <frawleyskid@gmail.com>
2021-12-18 10:38:22 -0500

    Add support for archiving sources to a CAR

---
+++ archive/car.go
@@ -0,0 +1,76 @@
+package archive
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-blockservice"
+	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-datastore"
+	syncds "github.com/ipfs/go-datastore/sync"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	offline "github.com/ipfs/go-ipfs-exchange-offline"
+	ipld "github.com/ipfs/go-ipld-format"
+	dag "github.com/ipfs/go-merkledag"
+	gocar "github.com/ipld/go-car"
+	gocarv2 "github.com/ipld/go-car/v2"
+	selectorparse "github.com/ipld/go-ipld-prime/traversal/selector/parse"
+	"io/ioutil"
+	"os"
+)
+
+var ErrCarAlreadyExists = errors.New("this file already exists")
+
+type dagStore struct {
+	service ipld.DAGService
+}
+
+func (ds dagStore) Get(ctx context.Context, c cid.Cid) (blocks.Block, error) {
+	return ds.service.Get(ctx, c)
+}
+
+func CarService() (ipld.DAGService, error) {
+	store := blockstore.NewBlockstore(syncds.MutexWrap(datastore.NewMapDatastore()))
+	blockService := blockservice.New(store, offline.Exchange(store))
+	return dag.NewDAGService(blockService), nil
+}
+
+func WriteCar(ctx context.Context, service ipld.DAGService, root cid.Cid, path string, carv2 bool) error {
+	if _, err := os.Stat(path); !errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("%w: %s", ErrCarAlreadyExists, path)
+	}
+
+	sourceDag := gocar.Dag{Root: root, Selector: selectorparse.CommonSelector_ExploreAllRecursively}
+	store := dagStore{service}
+	car := gocar.NewSelectiveCar(ctx, store, []gocar.Dag{sourceDag}, gocar.TraverseLinksOnlyOnce())
+
+	carFile, err := ioutil.TempFile("", "ipfs-bib-*.car")
+	if err != nil {
+		return err
+	}
+
+	if err := car.Write(carFile); err != nil {
+		return err
+	}
+
+	if err := carFile.Close(); err != nil {
+		return err
+	}
+
+	if carv2 {
+		if err := gocarv2.WrapV1File(carFile.Name(), path); err != nil {
+			return err
+		}
+
+		if err := os.Remove(carFile.Name()); err != nil {
+			return err
+		}
+	} else {
+		if err := os.Rename(carFile.Name(), path); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
--- archive/store.go
+++ archive/store.go
@@ -1,7 +1,83 @@
 package archive
 
-import "context"
+import (
+	"context"
+	"github.com/ipfs/go-cid"
+	ipld "github.com/ipfs/go-ipld-format"
+	dag "github.com/ipfs/go-merkledag"
+	unixfs "github.com/ipfs/go-unixfs/io"
+	"io"
+)
 
-type SourceStore interface {
-	AddSource(ctx context.Context, source *BibSource) (id *BibSourceId, err error)
+type SourceStore struct {
+	service   ipld.DAGService
+	directory unixfs.Directory
+}
+
+func NewSourceStore(ctx context.Context, service ipld.DAGService) (*SourceStore, error) {
+	directory := unixfs.NewDirectory(service)
+
+	dirNode, err := directory.GetNode()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := service.Add(ctx, dirNode); err != nil {
+		return nil, err
+	}
+
+	return &SourceStore{service, directory}, nil
+}
+
+func (s *SourceStore) Write(ctx context.Context) (cid.Cid, error) {
+	node, err := s.directory.GetNode()
+	if err != nil {
+		return cid.Undef, err
+	}
+
+	if err := s.service.Add(ctx, node); err != nil {
+		return cid.Undef, err
+	}
+
+	return node.Cid(), nil
+}
+
+func (s *SourceStore) AddSource(ctx context.Context, source *BibSource) (id *BibSourceId, err error) {
+	defer func() {
+		err = source.Content.Close()
+	}()
+
+	sourceData, err := io.ReadAll(source.Content)
+	if err != nil {
+		return nil, err
+	}
+
+	contentNode := dag.NewRawNode(sourceData)
+	if err := s.service.Add(ctx, contentNode); err != nil {
+		return nil, err
+	}
+
+	sourceDirectory := unixfs.NewDirectory(s.service)
+
+	if err := sourceDirectory.AddChild(ctx, source.FileName, contentNode); err != nil {
+		return nil, err
+	}
+
+	directoryNode, err := sourceDirectory.GetNode()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.service.Add(ctx, directoryNode); err != nil {
+		return nil, err
+	}
+
+	if err := s.directory.AddChild(ctx, source.DirectoryName, directoryNode); err != nil {
+		return nil, err
+	}
+
+	return &BibSourceId{
+		ContentCid:   contentNode.Cid(),
+		DirectoryCid: directoryNode.Cid(),
+	}, err
 }
