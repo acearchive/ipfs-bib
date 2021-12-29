93d830fc6324d2a8550057d6abdbcbf12b6741f1
Frawley <frawleyskid@gmail.com>
2021-12-29 16:08:43 -0500

    Avoid hardcoding IPFS path

--- store/pin.go
+++ store/pin.go
@@ -7,11 +7,11 @@ import (
 	shell "github.com/ipfs/go-ipfs-api"
 	ipfs "github.com/ipfs/go-ipfs-http-client"
 	"github.com/ipfs/interface-go-ipfs-core/options"
-	"github.com/ipfs/interface-go-ipfs-core/path"
+	ipfspath "github.com/ipfs/interface-go-ipfs-core/path"
 )
 
 func Pin(ctx context.Context, api *ipfs.HttpApi, id cid.Cid, recursive bool) error {
-	ipfsPath := path.IpfsPath(id)
+	ipfsPath := ipfspath.IpfsPath(id)
 	option := func(settings *options.PinAddSettings) error {
 		settings.Recursive = recursive
 
@@ -27,5 +27,5 @@ func Pin(ctx context.Context, api *ipfs.HttpApi, id cid.Cid, recursive bool) err
 
 func AddToMfs(ctx context.Context, apiUrl string, cid cid.Cid, path string) error {
 	api := shell.NewShell(apiUrl)
-	return api.FilesCp(ctx, fmt.Sprintf("/ipfs/%s", cid.String()), path)
+	return api.FilesCp(ctx, ipfspath.IpfsPath(cid).String(), path)
 }
